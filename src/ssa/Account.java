58616b51c67fe09b2381fc5f57d5892f18f21ee0
Peter <peterchoi1123@gmail.com>
2016-08-23 16:16:43 -0400

    Added my Updated Bank Account Program

--- src/ssa/Account.java
+++ src/ssa/Account.java
@@ -2,62 +2,92 @@ package ssa;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.UUID;
+//import java.util.UUID;
 
 public class Account {
 
-    int accountId; //local variable account ID for each object
-    String description; //local variable account ID for each object
+    private int accountId; //local variable account ID for each object
+    private String description; //local variable account ID for each object
     double balance; //local variable account ID for each object
-    DecimalFormat df = new DecimalFormat("$0.00"); //decimal format used for each $ amount
-    ArrayList<String> accountBalanceChanges = new ArrayList<String>(); //holds all past transactions for each object
+    private DecimalFormat df = new DecimalFormat("$0.00"); //decimal format used for each $ amount
+    //private ArrayList<String> accountBalanceChanges = new ArrayList<String>(); //holds all past transactions for each object
+    private static ArrayList<Integer> accountIdList = new ArrayList<Integer>();
+    private static int count = 1;
 
-    //constructor for setting each object's account ID and also adding the initial balance to the arraylist
-    public Account(double balance) {
-        this.balance = balance;
-        accountBalanceChanges.add("Starting Balance: " + df.format(getBalance()));
+    
+    public Account() {
+        setAccountId(count);
+        accountIdList.add(count);
+        count++;
+        
     }
-
+    
+    
+    public Account(String description) {
+        setAccountId(count);
+        setDescription(description);
+        accountIdList.add(count);
+        count++;
+    }
+    
+    public Account(int id, String description) {
+       
+        boolean duplicateFound = false;
+        
+      do { 
+        if(accountIdList.contains(id)) {
+            id++;
+            duplicateFound = true;
+        } else { 
+            setAccountId(id);
+            setDescription(description);
+            accountIdList.add(id);
+            duplicateFound = false;
+        }
+      } while(duplicateFound == true);
+       
+    }
+    
     
     //uses the checkTransfer method to see if transferring is possible with ">=0" constraints.
     //if transferring is possible, attaches a unique transaction id and transfers the $ amount from each account
     //adds the transaction to the arraylist
-    void transferBalance(double transferAmount, Account toAccount) {
-        String uniqueID = UUID.randomUUID().toString();
+    void transferFrom(Account toAccount, double transferAmount) {
+        //String uniqueID = UUID.randomUUID().toString();
         if (checkTransfer(transferAmount) == (true)) {
-            System.out.println("Transfering " + df.format(transferAmount) + " From - \"" + getDescription() + "\" to \""
-                    + toAccount.getDescription() + "\"");
-            this.balance -= transferAmount;
-            toAccount.balance += transferAmount;
-            System.out.println("Transaction ID: " + uniqueID + "\n");
-
-            accountBalanceChanges.add("Balance: " + df.format(getBalance()) + " (Transferred -"
-                    + df.format(transferAmount) + ") | Transaction ID: " + uniqueID);
-
-            toAccount.accountBalanceChanges.add("Balance: " + df.format(toAccount.getBalance()) + " (Transferred +"
-                    + df.format(transferAmount) + ") | Transaction ID: " + uniqueID);
+          //  System.out.println("Transfering " + df.format(transferAmount) + " From - \"" + toAccount.getDescription() + "\" to \""
+           //         + this.getDescription() + "\"");
+            toAccount.balance -= transferAmount;
+            this.balance += transferAmount;
+           // System.out.println("Transaction ID: " + uniqueID);
+           // System.out.println(
+           //         "\n***********************************************************************************************\n");
 
         } else {
-            System.out.println("Transfering " + df.format(transferAmount) + " From - \"" + getDescription() + "\" to \""
-                    + toAccount.getDescription() + "\" was canceled. Insufficient Funds!\n");
+         //   System.out.println("Transfering " + df.format(transferAmount) + " From - \"" + toAccount.getDescription() + "\" to \""
+          //          + this.getDescription() + "\" was canceled. Insufficient Funds!");
+          //  System.out.println(
+          //          "\n***********************************************************************************************\n");
         }
-
-        accountStatus();
-        System.out.println();
-        toAccount.accountStatus();
-        System.out.println(
-                "\n***********************************************************************************************\n");
 
     }
 
     //prints out the user's account status such as ID and balance
-    void accountStatus() {
-        System.out.println("User's Account ID : " + getAccountId() + "\nDescription: " + getDescription()
-                + "\nBalance: " + df.format(getBalance()));
+    String print() {
+        
+        return "Account " + getAccountId() + " balance is " + df.format(getBalance()); 
+
+        
+//        System.out.println("User's Account ID : " + getAccountId() + "\nDescription: " + getDescription()
+//                + "\nBalance: " + df.format(getBalance()));
+//        System.out.println(
+//                "\n***********************************************************************************************\n");
+
+    
     }
 
     //sets the account ID
-    void setAccountId(int accountId) {
+    private void setAccountId(int accountId) {
         this.accountId = accountId;
     }
 
@@ -67,7 +97,7 @@ public class Account {
     }
 
     //setting the account description
-    void setDescription(String description) {
+     void setDescription(String description) {
         this.description = description;
     }
 
@@ -85,29 +115,27 @@ public class Account {
     double getBalance() {
         return this.balance;
     }
+    
+    private void setBalance(double balance) {
+        this.balance = balance;
+    }
 
     //check if withdrawing the amount is possible from the account
     //if possible withdraw the amount and add it to the array list for past transactions
     //if impossible then println a message saying so
     void withdraw(double withdrawAmount) {
 
-        String uniqueID = UUID.randomUUID().toString();
+        //String uniqueID = UUID.randomUUID().toString();
         if (this.balance - withdrawAmount >= 0) {
             this.balance -= withdrawAmount;
-            System.out.println("Withdrawing... " + df.format(withdrawAmount));
-            System.out.println("Transaction ID: " + uniqueID);
-            accountBalanceChanges.add("Balance: " + df.format(getBalance()) + " (Withdrew -" + df.format(withdrawAmount)
-                    + ") | Transaction ID: " + uniqueID);
+           // System.out.println("Withdrawing... " + df.format(withdrawAmount));
+          //  System.out.println("Transaction ID: " + uniqueID);
 
         } else {
-            System.out.println("Withdrawing... " + df.format(withdrawAmount));
-            System.out.println("Insufficient Funds! Unable to withdraw " + df.format(withdrawAmount));
+           // System.out.println("Withdrawing... " + df.format(withdrawAmount));
+          //  System.out.println("Insufficient Funds! Unable to withdraw " + df.format(withdrawAmount));
 
         }
-
-        accountStatus();
-        System.out.println(
-                "\n***********************************************************************************************\n");
 
     }
 
@@ -127,27 +155,11 @@ public class Account {
     //deposit the desired amount to the account and add the transaction to the array list
     void deposit(double depositAmount) {
 
-        String uniqueID = UUID.randomUUID().toString();
-        System.out.println("Depositing... " + df.format(depositAmount));
-        System.out.println("Transaction ID: " + uniqueID);
+        //String uniqueID = UUID.randomUUID().toString();
+       // System.out.println("Depositing... " + df.format(depositAmount));
+       // System.out.println("Transaction ID: " + uniqueID);
         this.balance += depositAmount;
-        accountBalanceChanges.add("Balance: " + df.format(getBalance()) + " (Deposited +" + df.format(depositAmount)
-                + ") | Transaction ID: " + uniqueID);
-        accountStatus();
-        System.out.println(
-                "\n***********************************************************************************************\n");
-
-    }
-
-    //loop through the array list and display all of the past transactions
-    void checkBalanceHistory() {
-
-        System.out.println(this.description + " past transactions");
-
-        for (String recordedActions : accountBalanceChanges) {
-            System.out.println(recordedActions.toString());
-        }
-        System.out.println("");
+        
     }
 
 }
--- src/ssa/Mainline.java
+++ src/ssa/Mainline.java
@@ -5,35 +5,52 @@ public class Mainline {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        Account checking = new Account(500.00); //constructor for initial balance
-        checking.setAccountId(100); //setting account ID
-        checking.setDescription("My personal checking account"); //setting account descriptions
+        Account checking1 = new Account();
+        checking1.setDescription("My personal checking account");
+        checking1.balance = 800.00;
+        checking1.print();
+        checking1.deposit(300.00);
+        checking1.print();
+        checking1.withdraw(699.99);
+        checking1.print();
+        checking1.deposit(149.99);
+        checking1.print();
+        checking1.withdraw(950.00); // this should fail!
+        checking1.print();
+        checking1.withdraw(200.00);
+        checking1.print(); // balance should be 350
+        //
+        Account savings1 = new Account("My personal savings account");
+        savings1.deposit(1000.00);
+        savings1.print();
+        savings1.deposit(400.00);
+        savings1.print();
+        savings1.withdraw(750.00);
+        savings1.print();
+        savings1.withdraw(250.00);
+        savings1.print();
+        savings1.deposit(650.00);
+        savings1.print(); // balance should be 1050.00
+        // the transfer
+        checking1.transferFrom(savings1, 100.00); // transfer 100 from savings to checking
+        
+        // another account
+        Account checking2 = new Account("My slush fund checking account");
+        checking2.deposit(300.00);
+        checking2.print();
+        checking2.withdraw(150.00);
+        checking2.print();
+        checking2.deposit(250.00);
+        checking2.print();
 
-        Account savings = new Account(1000.00); //constructor for initial balance
-        savings.setAccountId(200);//setting account ID
-        savings.setDescription("My personal savings account");//setting account descriptions
+        // more to come. Will be updated during the lab...*
 
-        checking.deposit(200.00);
+        // print out all account balances
+        checking1.print();
+        checking2.print();
+        savings1.print();
+        
 
-        checking.withdraw(600.00);
-
-        checking.deposit(100.00);
-
-        checking.withdraw(300.00);
-
-        checking.withdraw(200.00);
-
-        savings.withdraw(750.00);
-
-        savings.withdraw(250.00);
-
-        savings.deposit(200.00);
-
-        savings.transferBalance(100.00, checking); //transferring $100.00 from savings to checking
-
-        checking.checkBalanceHistory(); //displays all past transactions for checking
-        savings.checkBalanceHistory(); //displays all past transactions for savings
-
-    }
+   }
 
 }
