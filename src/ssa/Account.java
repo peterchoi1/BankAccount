44d1c11483c4d3db508ba6702a7986d9e39b5291
Peter <peterchoi1123@gmail.com>
2016-08-24 16:04:43 -0400

    Assignment 7 that includes changes made & Savings class

--- src/ssa/Account.java
+++ src/ssa/Account.java
@@ -2,145 +2,134 @@ package ssa;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-//import java.util.UUID;
 
 public class Account {
 
-    private int id; //local variable account ID for each object
-    private String description;
-    private double balance; 
-    private DecimalFormat df = new DecimalFormat("$0.00"); //decimal format used for each $ amount
-    //private ArrayList<String> accountBalanceChanges = new ArrayList<String>(); //holds all past transactions for each object
-    private static ArrayList<Integer> accountIdList = new ArrayList<Integer>();
-    private static int count = 1;
-
+    private int id; // local variable for id
+    private String description; // local variable for description
+    private double balance; // local variable for description
     
+    // decimal format used for each $ amount
+    DecimalFormat df = new DecimalFormat("$0.00"); 
+    
+    // holds all the IDs in order to check for duplicate IDs
+    private static ArrayList<Integer> accountIdList = new ArrayList<Integer>();
+    
+    // used within the constructor for auto generating an Id                                              
+    private static int count = 1; 
+    
+    // this constructor automatically sets the account id
     public Account() {
         setId(count);
         accountIdList.add(count);
         count++;
-        
+
     }
-    
-    
+
+    // this constructor automatically sets the account id and takes a
+    // description input
     public Account(String description) {
         setId(count);
         setDescription(description);
         accountIdList.add(count);
         count++;
     }
-    
+
+    // this constructor takes inputs on id and description and checks if the id
+    // is not a duplicate id
+    // if it is a duplicate - one will be created for you
     public Account(int id, String description) {
-       
+
         boolean duplicateFound = false;
-        
-      do { 
-        if(accountIdList.contains(id)) {
-            id++;
-            duplicateFound = true;
-        } else { 
-            setId(id);
-            setDescription(description);
-            accountIdList.add(id);
-            duplicateFound = false;
-        }
-      } while(duplicateFound == true);
-       
+
+        do {
+            if (accountIdList.contains(id)) {
+                id++;
+                duplicateFound = true;
+            } else {
+                setId(id);
+                setDescription(description);
+                accountIdList.add(id);
+                duplicateFound = false;
+            }
+        } while (duplicateFound == true);
+
     }
-    
-    
-    //uses the checkTransfer method to see if transferring is possible with ">=0" constraints.
-    //if transferring is possible, attaches a unique transaction id and transfers the $ amount from each account
-    //adds the transaction to the arraylist
+
+    // uses the checkTransfer method to see if transferring is possible with
+    // ">=0" constraints.
+    // if transferring is possible, attaches a unique transaction id and
+    // transfers the $ amount from each account
+    // adds the transaction to the arraylist
     void transferFrom(Account toAccount, double transferAmount) {
-        //String uniqueID = UUID.randomUUID().toString();
         if (checkTransfer(transferAmount) == (true)) {
-          //  System.out.println("Transfering " + df.format(transferAmount) + " From - \"" + toAccount.getDescription() + "\" to \""
-           //         + this.getDescription() + "\"");
             toAccount.balance -= transferAmount;
             this.balance += transferAmount;
-           // System.out.println("Transaction ID: " + uniqueID);
-           // System.out.println(
-           //         "\n***********************************************************************************************\n");
 
         } else {
-         //   System.out.println("Transfering " + df.format(transferAmount) + " From - \"" + toAccount.getDescription() + "\" to \""
-          //          + this.getDescription() + "\" was canceled. Insufficient Funds!");
-          //  System.out.println(
-          //          "\n***********************************************************************************************\n");
         }
 
     }
 
-    //prints out the user's account status such as ID and balance
+    // prints out the user's account status such as ID and balance
     String print() {
-        
-        return "Account " + getId() + " balance is " + df.format(getBalance()); 
 
-        
-//        System.out.println("User's Account ID : " + getAccountId() + "\nDescription: " + getDescription()
-//                + "\nBalance: " + df.format(getBalance()));
-//        System.out.println(
-//                "\n***********************************************************************************************\n");
+        return "Account " + getId() + " Balance is: " + df.format(getBalance());
 
-    
     }
 
-    //sets the account ID
+    // sets the account ID
     private void setId(int id) {
         this.id = id;
     }
 
-    //returns the account ID
+    // returns the account ID
     int getId() {
         return this.id;
     }
 
-    //setting the account description
-     void setDescription(String description) {
+    // setting the account description
+    void setDescription(String description) {
         this.description = description;
     }
 
-    //returns the description
+    // returns the description
     String getDescription() {
         return this.description;
     }
 
-    //setting the initial balance
+    // setting the initial balance
     void setInitialBalance(double balance) {
         this.balance = balance;
     }
 
-    //returns the balance
+    // returns the balance
     double getBalance() {
         return this.balance;
     }
-    
+
+    // sets the balance
     private void setBalance(double balance) {
         this.balance = balance;
     }
 
-    //check if withdrawing the amount is possible from the account
-    //if possible withdraw the amount and add it to the array list for past transactions
-    //if impossible then println a message saying so
+    // check if withdrawing the amount is possible from the account
+    // if possible withdraw the amount and add it to the array list
+    // for past transactions
+    // if impossible then println a message saying so
     void withdraw(double withdrawAmount) {
 
-        //String uniqueID = UUID.randomUUID().toString();
         if (this.balance - withdrawAmount >= 0) {
             this.balance -= withdrawAmount;
-           // System.out.println("Withdrawing... " + df.format(withdrawAmount));
-          //  System.out.println("Transaction ID: " + uniqueID);
 
         } else {
-           // System.out.println("Withdrawing... " + df.format(withdrawAmount));
-          //  System.out.println("Insufficient Funds! Unable to withdraw " + df.format(withdrawAmount));
 
         }
 
     }
 
-    
-    //check if transferring is possible as a utility for the transferBalance() method
+    // check if transferring is possible as a utility for the transferBalance()
+    // method
     boolean checkTransfer(double transferAmount) {
 
         if (this.balance - transferAmount >= 0) {
@@ -151,15 +140,14 @@ public class Account {
 
     }
 
-    
-    //deposit the desired amount to the account and add the transaction to the array list
+    // deposit the desired amount to the account and add the transaction to the
+    // array list
     void deposit(double depositAmount) {
 
-        //String uniqueID = UUID.randomUUID().toString();
-       // System.out.println("Depositing... " + df.format(depositAmount));
-       // System.out.println("Transaction ID: " + uniqueID);
-        this.balance += depositAmount;
-        
+        if (depositAmount > 0) {
+            this.balance += depositAmount;
+        }
+
     }
 
 }
